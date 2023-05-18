c4b7e589be189df71e6dccad68d9efbbb28e5baa
NH <34393417+InnerQi@users.noreply.github.com>
2023-05-17 19:57:50 -0700

    Create main.py
    Python code

---
+++ main.py
@@ -0,0 +1,9 @@
+""" Air Conditioner Size Calculator 
+1. Enter the square footage of the space or building needing to be cooled.
+Note: the calculation assumes 8-foot ceilings.
+2. Next, divide the space's square footage by 500.
+3. Then multiply the number from #2 and 12,000. This is the number of BTU the system will need to remove to cool the area.
+4. For each person who works in the space, add 380 BTU. If the number varies, take an average
+5. Add an extra 1,000 BTU for each window.
+6. Add an extra 1,200 BTU for each kitchen.
+"""
